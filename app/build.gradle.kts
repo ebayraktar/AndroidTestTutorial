3ee25e7ce9fd19d700225a96ce535c37abe612eb
Emre BAYRAKTAR <emre.bayraktar@mobillium.com>
2024-02-22 19:43:05 +0300

    Add fragment testing dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
+    debugImplementation(libs.androidx.fragment.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
