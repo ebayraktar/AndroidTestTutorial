a5bacc0f95ba2042b97aed0109436024989f7df8
Emre BAYRAKTAR <emre.bayraktar@mobillium.com>
2024-02-22 17:56:08 +0300

    Add calculator page and extensions

---
+++ app/src/main/java/com/example/androidtesttutorial/utils/Calculator.kt
@@ -0,0 +1,34 @@
+package com.example.androidtesttutorial.utils
+
+/**
+ * Created by eBayraktar on 22.02.2024.
+ */
+class Calculator {
+    fun sum(
+        num1: Double,
+        num2: Double,
+    ): Double {
+        return num1 + num2
+    }
+
+    fun sub(
+        num1: Double,
+        num2: Double,
+    ): Double {
+        return num1 - num2
+    }
+
+    fun mul(
+        num1: Double,
+        num2: Double,
+    ): Double {
+        return num1 * num2
+    }
+
+    fun div(
+        num1: Double,
+        num2: Double,
+    ): Double {
+        return num1 / num2
+    }
+}
---
+++ app/src/main/java/com/example/androidtesttutorial/utils/EditTextExt.kt
@@ -0,0 +1,9 @@
+package com.example.androidtesttutorial.utils
+
+import android.widget.EditText
+
+/**
+ * Created by eBayraktar on 22.02.2024.
+ */
+
+fun EditText.getPureText() = text.toString()
